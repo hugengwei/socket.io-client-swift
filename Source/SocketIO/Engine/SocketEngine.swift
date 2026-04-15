a6b2fb2f976b277e090593247e98278eb59cb693
胡更伟 <hugw@tuwan.com>
2026-04-15 19:08:04 +0800

    回退这次提交代码（存在问题：Socket频繁断开重连） Update SocketEngine.swift
    Fix the issue where it takes over 60 seconds to close the socket after a network disconnection, and the problem where the server-side socket takes over 30 seconds to close when the server ends a connection
    提交：
    354ed7e5e4792de254bc6bda2c27211affbd0e40 [354ed7e]
    父级：
    175da8b515
    作者：
    Shaojie Hong <hsjcn@qq.com>
    日期：
    2024年5月9日 GMT+8 16:58:16
    提交者：
    GitHub <noreply@github.com>

--- Source/SocketIO/Engine/SocketEngine.swift
+++ Source/SocketIO/Engine/SocketEngine.swift
@@ -761,12 +761,6 @@ extension SocketEngine {
         case .disconnected(_, _):
             wsConnected = false
             websocketDidDisconnect(error: nil)
-        case .viabilityChanged(false):
-            wsConnected = false
-            websocketDidDisconnect(error: nil)
-        case .peerClosed:
-            wsConnected = false
-            websocketDidDisconnect(error: nil)
         case let .text(msg):
             parseEngineMessage(msg)
         case let .binary(data):
